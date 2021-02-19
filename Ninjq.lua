c257414cbfb1ca77693b864307928a083b3284de
sourceninjaiq <79315968+sourceninjaiq@users.noreply.github.com>
2021-02-19 17:41:53 +0300

    Add files via upload

---
+++ Ninjq.lua
@@ -0,0 +1,301 @@
+local function Ninjq(msg)
+local text = msg.content_.text_
+if not database:get(bot_id..'Ninjq:Status'..msg.chat_id_) then
+if text == 'هلو' then
+TextNinjq = 'هلوات يروحي🥺♥️'
+send(msg.chat_id_, msg.id_,'['..TextNinjq..']')
+return false
+end
+if text == 'شلونكم' then
+TextNinjq = 'تمام وانتا 🥺💘'
+send(msg.chat_id_, msg.id_,'['..TextNinjq..']')
+return false
+end
+if text == 'شلونك' then
+TextNinjq = 'مثل لونك ازرك🥺😂'
+send(msg.chat_id_, msg.id_,'['..TextNinjq..']')
+return false
+end
+if text == 'تمام' then
+TextNinjq = 'دوم يعمري🥺♥️'
+send(msg.chat_id_, msg.id_,'['..TextNinjq..']')
+return false
+end
+if text == 'هلاو' then
+TextNinjq = 'اهلا والف هلا🥺🔥'
+send(msg.chat_id_, msg.id_,'['..TextNinjq..']')
+return false
+end
+if text == '😐' then
+TextNinjq = 'شبيك صافن ولك😌👌'
+send(msg.chat_id_, msg.id_,'['..TextNinjq..']')
+return false
+end
+if text == 'هاي' then
+TextNinjq = 'هايات عمري💘😍'
+send(msg.chat_id_, msg.id_,'['..TextNinjq..']')
+return false
+end
+if text == 'بوت زربه' then
+TextNinjq = 'زربه بحلكك وليدي🌚😂'
+send(msg.chat_id_, msg.id_,'['..TextNinjq..']')
+return false
+end
+if text == 'اريد اكبل' then
+TextNinjq = 'شوفلي وياك وحده😂🥺'
+send(msg.chat_id_, msg.id_,'['..TextNinjq..']')
+return false
+end
+if text == 'لتزحف' then
+TextNinjq = 'لصير لوكي ونجب😪🤞'
+send(msg.chat_id_, msg.id_,'['..TextNinjq..']')
+return false
+end
+if text == 'كلخرا' then
+TextNinjq = 'خلي نفسك بماعون🌚😂'
+send(msg.chat_id_, msg.id_,'['..TextNinjq..']')
+return false
+end
+if text == 'زاحف' then
+TextNinjq = 'زاحف علي ختك 👐😹'
+send(msg.chat_id_, msg.id_,'['..TextNinjq..']')
+return false
+end
+if text == 'دي' then
+TextNinjq = 'ديصيحون ابوك🌚😂'
+send(msg.chat_id_, msg.id_,'['..TextNinjq..']')
+return false
+end
+if text == 'فرخ' then
+TextNinjq = 'ماكو غيرك فرخ😹💓'
+send(msg.chat_id_, msg.id_,'['..TextNinjq..']')
+return false
+end
+if text == 'تعالي خاص' then
+TextNinjq = 'هلو لصير زاحف ونجب😹👻'
+send(msg.chat_id_, msg.id_,'['..TextNinjq..']')
+return false
+end
+if text == 'اكرهك' then
+TextNinjq = 'شدعوه اني احبك🌚😂'
+send(msg.chat_id_, msg.id_,'['..TextNinjq..']')
+return false
+end
+if text == 'احبك' then
+TextNinjq = 'اني هم يروحي🥺🔥'
+send(msg.chat_id_, msg.id_,'['..TextNinjq..']')
+return false
+end
+if text == 'باي' then
+TextNinjq = 'الله وياك راحتن وخفتن😂😶'
+send(msg.chat_id_, msg.id_,'['..TextNinjq..']')
+return false
+end
+if text == 'واكف' then
+TextNinjq = 'شنو هوه واكف😶😑'
+send(msg.chat_id_, msg.id_,'['..TextNinjq..']')
+return false
+end
+if text == 'وين المدير' then
+TextNinjq = 'ليش محتاج شي منه 😉😁'
+send(msg.chat_id_, msg.id_,'['..TextNinjq..']')
+return false
+end
+if text == 'انجب' then
+TextNinjq = 'جب بحلكك ابني🙂😂'
+send(msg.chat_id_, msg.id_,'['..TextNinjq..']')
+return false
+end
+if text == 'تحبني' then
+TextNinjq = 'ما احب لوكيه😹🌚'
+send(msg.chat_id_, msg.id_,'['..TextNinjq..']')
+return false
+end
+if text == '🌚' then
+TextNinjq = 'فدوه هل سواد🥺😂'
+send(msg.chat_id_, msg.id_,'['..TextNinjq..']')
+return false
+end
+if text == '🙄' then
+TextNinjq = 'نزل عينك خواتك فوك😹💝'
+send(msg.chat_id_, msg.id_,'['..TextNinjq..']')
+return false
+end
+if text == '😒' then
+TextNinjq = 'شبيك ضايج يروحي😿👌'
+send(msg.chat_id_, msg.id_,'['..TextNinjq..']')
+return false
+end
+if text == '😳' then
+TextNinjq = 'لا بس شفت بريعصي😹💘'
+send(msg.chat_id_, msg.id_,'['..TextNinjq..']')
+return false
+end
+if text == '🙁' then
+TextNinjq = 'ها شنو عافتك حبيبتك 😉😂'
+send(msg.chat_id_, msg.id_,'['..TextNinjq..']')
+return false
+end
+if text == '🚶💔' then
+TextNinjq = 'ضايج الحلو 😆👋'
+send(msg.chat_id_, msg.id_,'['..TextNinjq..']')
+return false
+end
+if text == '🙂' then
+TextNinjq = 'ثكيل ابو زربه 😹🥺'
+send(msg.chat_id_, msg.id_,'['..TextNinjq..']')
+return false
+end
+if text == '🌝' then
+TextNinjq = 'منور حبيبي 💘😍'
+send(msg.chat_id_, msg.id_,'['..TextNinjq..']')
+return false
+end
+if text == 'صباحو' then
+TextNinjq = 'صباح العافيه🥺♥️'
+send(msg.chat_id_, msg.id_,'['..TextNinjq..']')
+return false
+end
+if text == 'صباح الخير' then
+TextNinjq = 'صباح الخير ياطير🥺😂'
+send(msg.chat_id_, msg.id_,'['..TextNinjq..']')
+return false
+end
+if text == 'كفو' then
+TextNinjq = 'منك حبيبي😉👌'
+send(msg.chat_id_, msg.id_,'['..TextNinjq..']')
+return false
+end
+if text == '😌' then
+TextNinjq = 'مرتاح الزاحف🙊😹'
+send(msg.chat_id_, msg.id_,'['..TextNinjq..']')
+return false
+end
+if text == 'اها' then
+TextNinjq = 'يب حبي قابل اغشك😉👋'
+send(msg.chat_id_, msg.id_,'['..TextNinjq..']')
+return false
+end
+if text == 'شسمج' then
+TextNinjq = 'اسمها جاسميه😻😹'
+send(msg.chat_id_, msg.id_,'['..TextNinjq..']')
+return false
+end
+if text == 'شسمك' then
+TextNinjq = 'اسمه ابو راس 😻🤙'
+send(msg.chat_id_, msg.id_,'['..TextNinjq..']')
+return false
+end
+if text == 'شوف' then
+TextNinjq = 'ششوف حبي😑👋'
+send(msg.chat_id_, msg.id_,'['..TextNinjq..']')
+return false
+end
+if text == 'مساء الخير' then
+TextNinjq = 'مساء النور 🥺♥️'
+send(msg.chat_id_, msg.id_,'['..TextNinjq..']')
+return false
+end
+if text == 'المدرسه' then
+TextNinjq = 'دنجب صاير شاطر😂🌚'
+send(msg.chat_id_, msg.id_,'['..TextNinjq..']')
+return false
+end
+if text == 'منو ديحذف رسائلي' then
+TextNinjq = 'خالتكك😹🤟'
+send(msg.chat_id_, msg.id_,'['..TextNinjq..']')
+return false
+end
+if text == 'البوت واكف' then
+TextNinjq = 'هياتني شغال لجذب'
+send(msg.chat_id_, msg.id_,'['..TextNinjq..']')
+return false
+end
+if text == 'غلس' then
+TextNinjq = 'علي شنو اغلس 🌚😂'
+send(msg.chat_id_, msg.id_,'['..TextNinjq..']')
+return false
+end
+if text == 'حارة' then
+TextNinjq = 'اي والله حيل حاره🥵🤯'
+send(msg.chat_id_, msg.id_,'['..TextNinjq..']')
+return false
+end
+if text == 'هههه' then
+TextNinjq = 'دومها حبي 🤟😻'
+send(msg.chat_id_, msg.id_,'['..TextNinjq..']')
+return false
+end
+if text == 'ههههه' then
+TextNinjq = 'فدوه لهل ضحكه 🥺😍'
+send(msg.chat_id_, msg.id_,'['..TextNinjq..']')
+return false
+end
+if text == '😹' then
+TextNinjq = 'قرد ويضحك😂👐'
+send(msg.chat_id_, msg.id_,'['..TextNinjq..']')
+return false
+end
+if text == 'وين' then
+TextNinjq = 'افتر ورجع😹🤟'
+send(msg.chat_id_, msg.id_,'['..TextNinjq..']')
+return false
+end
+if text == 'كافي لغوة' then
+TextNinjq = 'انتا انجب😹🤟'
+send(msg.chat_id_, msg.id_,'['..TextNinjq..']')
+return false
+end
+if text == 'نايمين' then
+TextNinjq = 'امشي كعدهم 😹😑'
+send(msg.chat_id_, msg.id_,'['..TextNinjq..']')
+return false
+end
+if text == 'اكو احد' then
+TextNinjq = 'اني موجود 😉👋'
+send(msg.chat_id_, msg.id_,'['..TextNinjq..']')
+return false
+end
+if text == 'فديت' then
+TextNinjq = 'فداك/ج ثولان العالم😹💝'
+send(msg.chat_id_, msg.id_,'['..TextNinjq..']')
+return false
+end
+if text == 'شكو' then
+TextNinjq = 'كلشي اكو سمج دجاج شتريد اكو 😹🤸‍♀️'
+send(msg.chat_id_, msg.id_,'['..TextNinjq..']')
+return false
+end
+if text == 'اوف' then
+TextNinjq = 'اوفات حبي😻💘'
+send(msg.chat_id_, msg.id_,'['..TextNinjq..']')
+return false
+end
+if text == 'احبج' then
+TextNinjq = 'دنجب لوكي🥺😻'
+send(msg.chat_id_, msg.id_,'['..TextNinjq..']')
+return false
+end
+if text == 'انتة منو' then
+TextNinjq = 'اني تاج راسك👑🥺'
+send(msg.chat_id_, msg.id_,'['..TextNinjq..']')
+return false
+end
+end
+
+if text == 'تفعيل ردود البوت' and Manager(msg) then
+database:del(bot_id..'Ninjq:Status'..msg.chat_id_)
+send(msg.chat_id_, msg.id_,'🚸| تم تفعيل ردود البوت')
+return false
+end
+
+if text == 'تعطيل ردود البوت' and Manager(msg) then
+database:set(bot_id..'Ninjq:Status'..msg.chat_id_,true)
+send(msg.chat_id_, msg.id_,'🔰| تم تعطيل ردود البوت')
+return false
+end
+
+end
+return {
+ninjaIQ = Ninjq
+}
